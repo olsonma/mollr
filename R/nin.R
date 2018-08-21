332b1ab52decb4da5d90d335496c5ea76506f119
Molly Olson <molly.olson@vanderbilt.edu>
2018-08-21 11:02:24 -0500

    update nin

--- R/nin.R
+++ R/nin.R
@@ -5,10 +5,12 @@
 #' @param x vector: the values to be matched
 #' @param y vector, list, or data frame: the value to be matched against
 #'
-#'
 #' @keywords render
+#' @return boolean
 #' @export
 #' @examples
 #' # Not run:
 #'
-'%nin%' <- function(x,y) !('%in%'(x,y))
+#'
+nin <- function(x, table) !match(x, table, nomatch = 0L) > 0L
+'%nin%' <- function(x,y) (nin(x,y))
