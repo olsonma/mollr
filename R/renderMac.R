da11ae92bc0c385b35e278c82b5ec91532f409b0
Molly Olson <molly.olson@vanderbilt.edu>
2018-08-21 10:35:01 -0500

    add package, first push

---
+++ R/renderMac.R
@@ -0,0 +1,19 @@
+#' renderMac
+#'
+#' Renders file on mounted drive when running on a Mac. Must set working directory to file location.
+#'
+#' Rendered file will be put in "~/Desktop"
+#' @param filename Character string that is the file name.
+#' @details Must set working directory to file location.
+#'
+#'
+#' @keywords render
+#' @export
+#' @examples
+#' # Not run:
+
+renderMac <- function(filename){
+  if(getwd() %in% "/Users/mollyolson") stop("Must set working directory to file location")
+  if(filename %nin% list.files()) stop("File doesn't exist in working directory")
+  rmarkdown::render(input = paste0(getwd(),"/",filename), output_dir = "~/Desktop", intermediates_dir = "~/Desktop")
+}
